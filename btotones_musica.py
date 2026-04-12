c722e31c737968a452876e07b997100b07bc3311
jhmezzomo-hub <jmezzomo@alumno.huergo.edu.ar>
2026-04-11 23:49:16 -0300

    arreghle la distribucion delos botones

--- btotones_musica.py
+++ btotones_musica.py
@@ -4,24 +4,24 @@ from cargar_imagen import cargar_imagen
 from PIL import ImageTk
 
 def play(panel):
-    play_bt = tb.Button(panel, text="Play", style="success.TButton",command=reproducir)
-    play_bt.grid(column=2, row=2, padx=10, pady=10, ipadx=5, ipady=5)
+    play_bt = tb.Button(panel, text="Play ", style="success.TButton",command=reproducir)
+    play_bt.grid(column=2, row=3, sticky="nsew", padx=10, pady=10, ipadx=5, ipady=5)
     return play_bt
 
 def pause(panel):
-    pause_bt = tb.Button(panel, text="Pausa", style="secondary.TButton", command=pausar)
-    pause_bt.grid(column=1, row=2, sticky="nswe", padx=10, pady=10, ipadx=5, ipady=5)
+    pause_bt = tb.Button(panel, text="Pause", style="secondary.TButton", command=pausar)
+    pause_bt.grid(column=1, row=3, sticky="nsew", padx=10, pady=10, ipadx=5, ipady=5)
     return pause_bt
 
 def stop(panel):
     frenar_bt = tb.Button(panel, text="Stop", style="primary.TButton", command=frenar)
-    frenar_bt.grid(column=0, row=2, sticky="nswe", padx=10, pady=10, ipadx=5, ipady=5)
+    frenar_bt.grid(column=0, row=3, sticky="nsew", padx=10, pady=10, ipadx=5, ipady=5)
     return frenar_bt
 
 def imagen(panel):
     portada = cargar_imagen()
     portada = ImageTk.PhotoImage(portada)
-    img = tb.Canvas(panel, relief="raised", background="white", width=300, height=300, bd=15)
+    img = tb.Canvas(panel, relief="raised", background="#FFFFFF", width=300, height=300, bd=15)
     img.create_image(0, 0, anchor="nw", image=portada)
     img.grid(column=0, row=0, columnspan=3)
     img.reference = portada
--- cargar_imagen.py
+++ cargar_imagen.py
@@ -6,13 +6,13 @@ from pathlib import Path
 def cargar_imagen():
     imagen = obtener_info
     try:
-        new_imagen = Image.open(imagen[3])
+        new_imagen = Image.open(imagen[3]).convert("RGBA")
         new_imagen = imagen.resize((300, 300))
         return new_imagen
     except Exception as e:
         print(f"No se pudo cargar la imagen: {e}")
         folder =  Path(__file__).parent
         pred = folder / "album_predeterminado.png"
-        pred = Image.open(pred)
+        pred = Image.open(pred).convert("RGBA")
         album = pred.resize((300,300))
         return album
